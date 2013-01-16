31cd5221cb68106e123fcd614978c4cce54541d2
renan.alexs@gmail.com <renan.alexs@gmail.com@c2b38c94-9abe-f54a-276c-c2d33a108bd7>
2013-01-16 15:05:13 +0000

    BT 提示音

--- trunk/IBC233B/APP/decode_otp/decode_otp.c
+++ trunk/IBC233B/APP/decode_otp/decode_otp.c
@@ -33,7 +33,7 @@ extern u16 f_read (u8 _xdata *buffer, u16 len);
 extern u8 music_type;
 extern xd_u8 eq_mode;
 extern xd_u8 play_status;
-extern xd_u8 decode_cmd;
+extern u8 decode_cmd;
 
 extern u8 code_otp_read(u8 _xdata *buf, u16 lbah, u16 lbal);
 
@@ -42,27 +42,33 @@ void decode_opt_music_file(void)
 {
 	u8 key;
 
-    dsp_hi_pro();
-    decodeint_hi_pro();
+    	dsp_hi_pro();
+       decodeint_hi_pro();
 
 	f_init(win_buffer, (u16)code_otp_read, 1);
 	f_open((u8 *)&fat_file, 0);
-	f_open_dummy_file((u16)&_CONST_MyMusic_L_);
+	f_open_dummy_file((u16)&music_file);
 
-    mad_control(0,(u16)buffer);
-    set_eq(0);                             //必须在每首歌曲播放前初始化EQ
-    music_type = musice_format_check(buffer);
+    	mad_control(0,(u16)buffer);
+    	set_eq(0);                             //必须在每首歌曲播放前初始化EQ
+    	music_type = musice_format_check(buffer);
+		
+	dac_out_select(DAC_DECODE);
 	set_max_vol(MAX_ANALOG_VOL, MAX_DIGITAL_VOL);
-    my_memset(win_buffer, 0, 512);              //解码前，必须清除buffer
+    	my_memset(win_buffer, 0, 512);              //解码前，必须清除buffer
 	main_vol_set(0, SET_USE_CURRENT_VOL);
-    flush_low_msg();
-    play_status = MUSIC_PLAY;
-    decode_cmd = 0;
-    decode_user_exit(0);
+    	flush_low_msg();
+    	play_status = MUSIC_PLAY;
+   	decode_cmd = 0;
+    	decode_user_exit(0);
 	enable_softint();
-    enable_decode_isr();
+    	enable_decode_isr();
 	set_device(0);
-    while(1)
+
+	set_sys_vol(15);
+
+
+	while(1)
 	{
 	    key = get_msg();
 		if((DECODE_MSG_DISK_ERR == key) || (DECODE_MSG_FILE_END == key))
